dc98760c7ceb9d5b28fd3fdabc66cfcbd6178671
Flory Muenge <flory.contato@gmail.com>
2024-05-20 22:52:43 +0000

    inicinado o projeto e add header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1>Sym Finance</h1>
+        <button>
+          Nova transação
+        </button>
+      </header>
+    </div>
+  )
+}
+
+export default App
